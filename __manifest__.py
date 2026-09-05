5e657c81e934b9d6c8a623fb8d803a30adbc7177
Arsham Zarei <arsham.zarei1378@gmile.com>
2026-09-05 13:14:23 +0330

    start project

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "company",
+    'version': '17.0.0.1',
+    'depends': ['base'],
+    'author': "",
+    'category': 'اداری/مالی',
+    'description': """      """,
+    'license': 'LGPL-3',
+    'application': True,
+    'installable': True,
+    'data': [
+        'security/estate_security.xml',
+        'security/ir.model.access.csv',
+
+
+        'views/company_menus.xml',
+
+            ],
+}
